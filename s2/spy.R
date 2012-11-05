4f9a3690258b8635a1f0bcf220d478c4a884a2cb
Thanh Doan <tcdoan@gmail.com>
2012-11-05 00:40:40 -0600

    compute cond prob table

--- s2/spy.R
+++ s2/spy.R
@@ -4,23 +4,37 @@ getSymbols("SPY",src="yahoo")
 barChart(SPY)
 
 spy.returns = periodReturn(SPY,period='daily')
-dim(spy.returns)
+ndays = dim(spy.returns)[1]
 
-ndays = dim(spy_returns)[1]
-day.returns = data.frame(spy.returns[-1,])
-dim(day.returns)
+today.returns = spy.returns
+today.returns[1,] = NA
+today.returns = subset(today.returns, !is.na(today.returns))
 
-prev.returns =  spy.returns[1:ndays,]
-dim(prev.returns)
+prev.returns = spy.returns
+prev.returns[ndays,] = NA
+prev.returns = subset(prev.returns, !is.na(prev.returns))
 
-day.returns$prev.returns = prev.returns
-day.returns[1:10,]
-spy.returns[1:10,]
+corr.returns = data.frame(today.returns)
+corr.returns$prev.returns = prev.returns
 
-day.returns$daily_bin =cut(day.returns$daily.returns, breaks = c(-1.0,-0.04,-0.02, 0.0, 0.02, 0.04, 1.0))
-day.returns$prev_bin  =cut(day.returns$prev.returns,  breaks = c(-1.0,-0.04,-0.02, 0.0, 0.02, 0.04, 1.0))
+names(corr.returns) = c("today.returns", "prev.returns")
+corr.returns[c(1:10, 1460:1469),]
 
-table(day.returns$prev_bin, day.returns$daily_bin)
+cor(corr.returns)
 
-  
+corr.returns$today_bins=cut(corr.returns$today.returns, breaks = c(-1.0,-0.04,-0.02, -0.01, 0.0, 0.01, 0.02, 0.04, 1.0))
+corr.returns$prev_bins =cut(corr.returns$prev.returns, breaks = c(-1.0,-0.04,-0.02, -0.01, 0.0, 0.01, 0.02, 0.04, 1.0))
+
+freq_table = table(corr.returns$prev_bins, corr.returns$today_bins)
+
+freq_table
+sum(freq_table)
+
+cond_prob = freq_table
+
+cond_prob = cond_prob/colSums(cond_prob)
+
+print(round(cond_prob,2))
+
+
 
