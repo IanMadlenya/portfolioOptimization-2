3a287596ae673a8b33ea830a4791ac55d55c3142
Thanh Doan <tcdoan@gmail.com>
2012-11-15 20:45:19 -0600

    MSCI HK index analysis

---
+++ s2/ewh.R
@@ -0,0 +1,42 @@
+## MSCI HK Index 
+
+library("quantmod")
+        
+getSymbols("EWH",src="yahoo")
+barChart(EWH)
+
+EWH.returns = periodReturn(EWH,period='daily')
+ndays = dim(EWH.returns)[1]
+
+today.returns = EWH.returns
+today.returns[1,] = NA
+today.returns = subset(today.returns, !is.na(today.returns))
+
+prev.returns = EWH.returns
+prev.returns[ndays,] = NA
+prev.returns = subset(prev.returns, !is.na(prev.returns))
+
+corr.returns = data.frame(today.returns)
+corr.returns$prev.returns = prev.returns
+
+names(corr.returns) = c("today.returns", "prev.returns")
+
+cor(corr.returns)
+
+corr.returns$today_bins=cut(corr.returns$today.returns, breaks = c(-1.0,-0.04,-0.02, -0.01, 0.0, 0.01, 0.02, 0.04, 1.0))
+corr.returns$prev_bins =cut(corr.returns$prev.returns, breaks = c(-1.0,-0.04,-0.02, -0.01, 0.0, 0.01, 0.02, 0.04, 1.0))
+
+freq_table = table(corr.returns$prev_bins, corr.returns$today_bins)
+
+freq_table
+
+sum(freq_table)
+
+cond_prob = freq_table
+
+cond_prob = cond_prob/colSums(cond_prob)
+
+print(round(cond_prob,2))
+
+
+
